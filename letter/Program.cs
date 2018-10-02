33c9c0e550d0cd40c8b832107fe435ee8d8815e7
pqo1993 <pqoalla@gmail.com>
2018-10-02 12:41:58 +0400

    Letter

---
+++ letter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("letter");
+            char y = char.Parse(Console.ReadLine());
+            if (char.IsLower(y))
+            {
+                y = char.ToUpper(y);
+                Console.WriteLine(y);
+            }
+            else
+            {
+                
+                Console.WriteLine(y);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
