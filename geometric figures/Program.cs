a3d83939f48f3240c2ad330980b47c9f36181780
pqo1993 <pqoalla@gmail.com>
2018-10-04 20:24:27 +0400

    geometric figurs

--- geometric figures/Program.cs
+++ geometric figures/Program.cs
@@ -13,15 +13,17 @@ namespace geometric_figures
         {
             Console.WriteLine("Choose your geometric figures:\nif square press-1\nif circle press-2\nif rectangle press-3 ");
             byte figur = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine("enter figurs size ");
-            if (figur == 1 || figur == 3)
-            { int A = Convert.ToInt32(Console.ReadLine()); int B = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("figur area is " + (A * B)); }
-            else if (figur == 2)
-            { Double R = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("figur are is " + (3.14 * Math.Pow(R,2))); }
-            Console.ReadKey();
+            if (figur == 1|| figur==3)
+            {   Console.WriteLine("enter figurs size ");
+                int A = Convert.ToInt32(Console.ReadLine());
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("area is "+A*B);}
+                else  if (figur==2)
+            {  double R = Convert.ToInt32(Console.ReadLine());
+                 Console.WriteLine("area is"+ 3.14* Math.Pow(R,2));}
+                else 
+                { Console.WriteLine("wrong"); }
+                Console.ReadKey(); }
         }
     }
 }
-    
