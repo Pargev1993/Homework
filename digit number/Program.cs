0984fc2ba46d444e6d19868bb26d9a502f114b49
pqo1993 <pqoalla@gmail.com>
2018-10-05 16:31:41 +0400

    Digit numbers

---
+++ digit number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_figures
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a four-digit number ");
+            int digitnumber = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            if (digitnumber >= 1000 && digitnumber <= 9999)
+            {
+                int num_1 = digitnumber / 1000;
+                int num_2 = digitnumber % 1000 / 100;
+                int num_3 = digitnumber % 1000 % 100 / 10;
+                int num_4 = digitnumber % 1000 % 100 % 10;
+                result = num_1 + num_2 + num_3 + num_4;
+                Console.WriteLine("result=" + result);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
